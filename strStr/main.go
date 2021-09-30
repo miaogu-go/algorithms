ae5c8837a663b969e0a081fc3083dd63aa811995
蒲强 <pu.qiang@qq.com>
2021-09-30 11:06:35 +0800

    第一次出现位置

---
+++ strStr/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	pos := StrStr2("aaaaa", "bba")
+	fmt.Println(pos)
+
+}
+
+func StrStr2(haystack, needle string) int {
+	if len(needle) == 0 {
+		return 0
+	}
+	for i := 0; i < len(haystack); i++ {
+		if len(needle)+i > len(haystack) {
+			return -1
+		}
+		if haystack[i:len(needle)+i] == needle {
+			return i
+		}
+	}
+	return -1
+}
+
+func StrStr(haystack, needle string) int {
+	if len(needle) == 0 {
+		return 0
+	}
+	i, j := 0, 0
+	length := len(haystack) - len(needle) + 1
+	for i = 0; i < length; i++ {
+		for j = 0; j < len(needle); j++ {
+			if haystack[i+j] != needle[j] {
+				break
+			}
+		}
+		if len(needle) == j {
+			return i
+		}
+	}
+	return -1
+}
