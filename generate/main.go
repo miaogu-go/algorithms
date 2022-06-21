87fb0770a50b670f415eb7ebbff67adb86ebde57
puqiang <puqiang@comisys.net>
2022-06-21 17:32:14 +0800

    杨辉三角

---
+++ generate/main.go
@@ -0,0 +1,60 @@
+package main
+
+import "fmt"
+
+//https://leetcode.cn/problems/pascals-triangle/
+
+func main() {
+	res := generate(5)
+	fmt.Println(res)
+	res = generate(1)
+	fmt.Println(res)
+
+	res = generate2(5)
+	fmt.Println(res)
+	res = generate2(1)
+	fmt.Println(res)
+}
+
+func generate(numRows int) [][]int {
+	result := make([][]int, 0)
+	for i := 1; i <= numRows; i++ {
+		if i <= 2 {
+			tmp := make([]int, 0)
+			for j := 0; j < i; j++ {
+				tmp = append(tmp, 1)
+			}
+			result = append(result, tmp)
+			continue
+		}
+
+		tmp := make([]int, i)
+		tmp[0], tmp[i-1] = 1, 1
+		//取出i-1的数组
+		prev := result[i-2]
+
+		x, y := 0, 1
+		for z := 1; z <= i-2; z++ {
+			tmp[z] = prev[x] + prev[y]
+			x++
+			y++
+		}
+		result = append(result, tmp)
+	}
+
+	return result
+}
+
+func generate2(numRows int) [][]int {
+	results := make([][]int, numRows)
+	for i := range results {
+		results[i] = make([]int, i+1)
+		results[i][0] = 1
+		results[i][i] = 1
+		for j := 1; j < i; j++ {
+			results[i][j] = results[i-1][j] + results[i-1][j-1]
+		}
+	}
+
+	return results
+}
