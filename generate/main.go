3566107469c09e780b9a886ba6f7e887f95ba860
puqiang <puqiang@comisys.net>
2022-06-21 17:41:27 +0800

    index

--- generate/main.go
+++ generate/main.go
@@ -14,6 +14,13 @@ func main() {
 	fmt.Println(res)
 	res = generate2(1)
 	fmt.Println(res)
+
+	result := getRow(0)
+	fmt.Println(result)
+	result = getRow(1)
+	fmt.Println(result)
+	result = getRow(3)
+	fmt.Println(result)
 }
 
 func generate(numRows int) [][]int {
@@ -57,4 +64,33 @@ func generate2(numRows int) [][]int {
 	}
 
 	return results
+}
+
+func getRow(rowIndex int) []int {
+	result := make([][]int, 0)
+	for i := 1; i <= rowIndex+1; i++ {
+		if i <= 2 {
+			tmp := make([]int, 0)
+			for j := 0; j < i; j++ {
+				tmp = append(tmp, 1)
+			}
+			result = append(result, tmp)
+			continue
+		}
+
+		tmp := make([]int, i)
+		tmp[0], tmp[i-1] = 1, 1
+		//取出i-1的数组
+		prev := result[i-2]
+
+		x, y := 0, 1
+		for z := 1; z <= i-2; z++ {
+			tmp[z] = prev[x] + prev[y]
+			x++
+			y++
+		}
+		result = append(result, tmp)
+	}
+
+	return result[rowIndex]
 }
